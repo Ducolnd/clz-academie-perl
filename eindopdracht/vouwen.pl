f512d4edf12a85f0fe9f596ceb69799e9d383801
Duco Lindhout <duco.lindhout@gmail.com>
2021-01-14 16:29:08 +0100

    Functie draggoncurve en jpg verschillende curves

--- eindopdracht/vouwen.pl
+++ eindopdracht/vouwen.pl
@@ -1,7 +1,9 @@
 use Math::Trig;
+use List::Util qw( min max );
+
+# use warnings;
 
 $l = "L";
-$r = "R";
 
 $order = $l;
 $new = "";
@@ -17,62 +19,91 @@ sub replace { # Vervang R door L en L door R
     return $out;
 }
 
-sub deg2rad {
+sub degress2radians {
     my ($degrees) = @_;
     return ($degrees / 180) * pi;
 }
 
-$iterations = 15;
+sub createdragon {
+    my ($n, $a, $sw) = (@_);
 
-for ((1..$iterations)) {
-    $new = $order;
+    print "Creating a dragoncurve with $n iterations, $a degree angles and $sw line width. \n";
 
-    $order = $order . $l . reverse(replace($new));
-}
+    $iterations = $n;
 
+    for ((0..$iterations)) {
+        $new = $order;
 
-my @coords = ([450,700]);
-$angle = 0;
-$segment_length = 5;
-$angle_step = 91;
-
-foreach $char (split //, $order) {
-    $lastx = $coords[-1][0];
-    $lasty = $coords[-1][1];
-
-    if ($char eq $l) {
-        $angle = ($angle + $angle_step) % 360;
-    } else {
-        $angle = ($angle - $angle_step) % 360;
+        $order = $order . $l . reverse(replace($new));
     }
 
-    $newx = $lastx + (sprintf("%.10f", cos(deg2rad($angle))) * $segment_length); # Sprintf rond de float's af naar 10 decimalen. Dit gaf eerst problemen omdat PI niet exact was.
-    $newy = $lasty + (sprintf("%.10f", sin(deg2rad($angle))) * $segment_length);
+    # print "$order \n";
 
-    # print "Angle: $angle X: $newx Y: $newy \n";
 
-    push @coords, [$newx, $newy];
+    $startx = 450;
+    $starty = 700;
+
+    my @coords = ([$startx,$starty]);
+    $angle = 0;
+    $segment_length = 5;
+    $angle_step = $a;
+
+    foreach $char (split //, $order) {
+        $lastx = $coords[-1][0];
+        $lasty = $coords[-1][1];
+
+        if ($char eq $l) {
+            $angle = ($angle + $angle_step) % 360;
+        } else {
+            $angle = ($angle - $angle_step) % 360;
+        }
+
+        $newx = $lastx + (sprintf("%.10f", cos(degress2radians($angle))) * $segment_length); # Sprintf rond de float's af naar 10 decimalen. Dit gaf eerst problemen omdat PI niet exact was.
+        $newy = $lasty + (sprintf("%.10f", sin(degress2radians($angle))) * $segment_length);
+
+        # print "Angle: $angle X: $newx Y: $newy \n";
+
+        push @coords, [$newx, $newy];
+    }
+
+    my @xes = map{ $_->[0]} @coords;
+    my @yes = map{ $_->[1]} @coords;
+
+    my $xmin = min @xes;
+    my $xmax = sprintf("%.0f", (max @xes) - $xmin);
+
+    my $ymin = min @yes;
+    my $ymax = sprintf("%.0f", (max @yes) - $ymin);
+
+
+    @xes = map{ $_ - $xmin} @xes;
+    @yes = map {$_ - $ymin} @yes;
+
+    $sstartx = sprintf("%.0f", $startx - $segment_length - $xmin);
+    $sstarty = sprintf("%.0f", $starty - $ymin);
+
+
+    $coord_string = "M " . "$sstartx" . ", " . "$sstarty" . " L ";
+
+    for(my $m = 0; $m <= $#coords; $m++) {  
+        $coord_string = $coord_string . sprintf("%.0f", $xes[$m]) . "," . sprintf("%.0f", $yes[$m]) . " ";
+    }  
+
+
+    $svg = "<?xml version='1.0' encoding='UTF-8' standalone='no'?><svg height='$ymax' width='$xmax'><g><path style='fill:none;stroke:#000000;stroke-width:2px' d='";
+    $svg2 = "'/></g></svg>";
+    $svg_full = $svg . $coord_string . $svg2;
+
+    unless(-e $a or mkdir $a) {
+        die "Unable to create folder $a\n";
+    }
+
+    open(file, ">", "$a/iteration_$n.svg");
+    print file $svg_full;
+    close(file);
 }
 
-$coord_string = "M 445,700 L ";
-
-for(my $m = 0; $m <= $#coords; $m++) {    
-   for(my $n = 0; $n <= 1 ; $n++) {   
-       $coord_string = $coord_string . sprintf("%.0f", $coords[$m][$n]) . ",";
-    }   
-
-    chop($coord_string);
-    $coord_string = $coord_string . " ";
-}  
+createdragon(6,90.5,2);
+# createdragon(2, 90, 2);
 
 
-
-$svg = "<?xml version='1.0' encoding='UTF-8' standalone='no'?><svg height='2000' width='2000' style='background-color:green;'><g><path style='fill:none;stroke:#000000;stroke-width:2px' d='";
-
-$svg2 = "'/></g></svg>";
-
-$svg_full = $svg . $coord_string . $svg2;
-
-open(file, ">", "dragoncurve.svg");
-print file $svg_full;
-close(file);
